a6213c09e9e332a68a3689f23fb1b480ef427945
unkuseni <amudalatadeoye@gmail.com>
2023-05-06 00:16:24 +0100

    setting up the homepage

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -10,13 +10,8 @@ export interface ButtonProps {
     sizes?: 'large' | 'medium' | 'small';
     text?: string;
     color?: 'Primary' | 'Secondary' | 'Danger' | 'Default';
-    icons?: string;
+    icons?: boolean;
 }
-
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-buttons-and-templates
- */
 export const Button: React.FC<ButtonProps> = ({
     className,
     disable,
@@ -37,13 +32,15 @@ export const Button: React.FC<ButtonProps> = ({
         [styles.primary]: color === 'Primary',
         [styles.secondary]: color === 'Secondary',
         [styles.danger]: color === 'Danger',
+        [styles.left]: iconPos === 'left',
+        [styles.right]: iconPos === 'right',
     });
 
     return (
-        <div className={`${className}`}>
-            {/* pass in font awesome as an icon prop */}
-            <button disabled={disable} className={classNames(classes, styles.root)}>
-                <FontAwesomeIcon icon={faCartShopping} className={styles.root} />
+        <div className={classNames(className, styles.root)}>
+            <button disabled={disable} className={classNames(classes, styles.default)}>
+                {/* pass in an icon */}
+                {icons && <FontAwesomeIcon icon={faCartShopping} />}
                 {text}
             </button>
         </div>
