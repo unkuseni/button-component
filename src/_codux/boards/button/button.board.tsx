45ac5a63911dea275917ce4ef206e5fa201478d3
Adeoye Olaseni <amudalatadeoye@gmail.com>
2023-05-06 13:12:31 +0100

    Delete src/_codux directory

--- src/_codux/boards/button/button.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Button } from '../../../components/button/button';
-
-export default createBoard({
-    name: 'Button',
-    Board: () => <Button text={'Default'} icons={true} />,
-});
